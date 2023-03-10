eafd1ba1486dd47dc66e2ee90a74b77d52f9cb8a
Kristina Orlichenko <chrisorlichenko@gmail.com>
2023-03-10 14:54:40 +0000

    change model for booking

--- booking/models.py
+++ booking/models.py
@@ -1,33 +1,58 @@
 from django.db import models
-from datetime import datetime
+from enum import unique
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
-SERVICE_CHOICES = (
-    ("First consultation", "First consultation"),
-    ("Consultation (Criminal law)", "Consultation (Criminal law)"),
-    ("Consultation (Civil law)", "Consultation (Civil law)"),
-    ("Consultation (Business)", "Consultation (Business)"),
-    )
-TIME_CHOICES = (
-    ("9 AM", "9 AM"),
-    ("10 AM", "10 AM"),
-    ("11 AM", "11 AM"),
-    ("12 PM", "12 PM"),
-    ("1 PM", "1 PM"),
-    ("2 PM", "2 PM"),
-    ("3 PM", "3 PM"),
-    ("4 PM", "4 PM"),
-    ("5 PM", "5 PM"),
-    ("6 PM", "6 PM"),
+
+class Service(models.Model):
+    """The model is used to display data about the services
+    provided by the lawfirm
+    """
+    service_name = models.CharField(max_length=150, unique=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    class Meta:
+        unique_together = ('service_name', 'price', )
+        ordering = ['-price']
+
+    def __str__(self):
+        return f"{self.service_name}, {self.price}"
+
+
+HOURS = (
+    ('10:00', '10:00'),
+    ('11:00', '11:00'),
+    ('12:00', '12:00'),
+    ('13:00', '13:00'),
+    ('14:00', '14:00'),
+    ('15:00', '15:00'),
+    ('16:00', '16:00'),
+    ('17:00', '17:00'),
+    ('18:00', '18:00'),
+    ('19:00', '19:00'),
 )
 
 
-class Appointment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    service = models.CharField(max_length=50, choices=SERVICE_CHOICES, default="First consultation")
-    day = models.DateField(default=datetime.now)
-    time = models.CharField(max_length=10, choices=TIME_CHOICES, default="9 AM")
-    time_ordered = models.DateTimeField(default=datetime.now, blank=True)
+class Booking(models.Model):
+
+    """Model that is used to store the data that the user enters in the
+    booking form
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, null=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=50)
+    email = models.EmailField(blank=True, null=True)
+    phoneRegex = RegexValidator(regex=r'^\+?1?\d{8,15}$',
+                                message="Please enter a valid phone number,"
+                                "e.g. 123456789. Up to 15 digits allowed.",
+                                code="invalid")
+    phone = models.CharField(validators=[phoneRegex], max_length=16,
+                             null=True, blank=True)
+    date = models.DateField()
+    time = models.CharField(max_length=30, choices=HOURS, default='10:00')
+
+    class Meta:
+        unique_together = ('user', 'date', 'time', 'service')
 
     def __str__(self):
-        return f"{self.user.username} | day: {self.day} | time: {self.time}"
+        return self.name
