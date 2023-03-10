5bc7cd985de275c6d8abc4894629489586932d9b
Kristina Orlichenko <chrisorlichenko@gmail.com>
2023-03-10 14:26:16 +0000

    Add feature to like/unlike a post

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
 
+
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by("-created_on")
@@ -32,7 +33,7 @@ class PostDetail(View):
                 "comment_form": comment_form,
             },
         )
-    
+
     def post(self, request, slug, *args, **kwargs):
 
         queryset = Post.objects.filter(status=1)
@@ -64,3 +65,14 @@ class PostDetail(View):
             },
         )
 
+
+class PostLike(View):
+
+    def post(self, request, slug):
+        post = get_object_or_404(Post, slug=slug)
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
