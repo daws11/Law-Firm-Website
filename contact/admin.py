0e5ca247885149042250f3759fc215943e3a7bcb
Kristina Orlichenko <chrisorlichenko@gmail.com>
2023-03-20 15:37:04 +0000

    Add class in admin for contact form

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from daterangefilter.filters import DateRangeFilter
+from .models import ContactModel
 
-# Register your models here.
+
+@admin.register(ContactModel)
+class ContactModelAdmin(admin.ModelAdmin):
+    list_filter = (
+        'first_name',
+        'last_name',
+        'email',
+        'phone_number',
+        'created_date'
+        )
+    list_display = (
+        'user',
+        'first_name',
+        'last_name',
+        'email',
+        'phone_number',
+        'message',
+        'created_date')
+
+    search_fields = ['phone_number',]
+    list_filter = (('created_date', DateRangeFilter),)
