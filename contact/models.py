c77b460b40325099423d196ae4862f273488446e
Kristina Orlichenko <chrisorlichenko@gmail.com>
2023-03-20 15:32:30 +0000

    Add models for contact form

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ContactModel(models.Model):
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="contact_user",
+        null=True
+        )
+   
+    first_name = models.CharField(
+        max_length=50,
+        null=True
+        )
+    last_name = models.CharField(
+        max_length=50,
+        null=True
+        )
+    email = models.EmailField(
+        max_length=254,
+        default=""
+        )
+    phone_number = models.CharField(
+        max_length=16, 
+        null=True, 
+        blank=True)
+    message = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_date']
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}, {self.created_date}"
